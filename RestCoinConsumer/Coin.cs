e1643cdcaecd2cba8300dcb27e6400601ea48019
basicxd <mikail-fener@hotmail.com>
2019-04-03 12:04:22 +0200

    MIAskdla

---
+++ RestCoinConsumer/Coin.cs
@@ -0,0 +1,25 @@
+﻿namespace RestCoinConsumer
+{
+    public class Coin
+    {
+        public int Id { get; set; }
+        public string Genstand { get; set; }
+        public int Bud { get; set; }
+        public string Navn { get; set; }
+
+        public Coin(int id, string genstand, int bud, string navn)
+        {
+            Id = id;
+            Genstand = genstand;
+            Bud = bud;
+            Navn = navn;
+        }
+
+        public Coin() { }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Genstand)}: {Genstand}, {nameof(Bud)}: {Bud}, {nameof(Navn)}: {Navn}";
+        }
+    }
+}
--- RestCoinConsumer/Program.cs
+++ RestCoinConsumer/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace RestCoinConsumer
 {
     class Program
     {
+        private static async Task<IList<Coin>> GetAllCoins()
+        {
+            string Uri = "https://localhost:44347/api/coin";
+            using (HttpClient client = new HttpClient())
+            {
+                string content = await client.GetStringAsync(Uri);
+                IList<Coin> coinList = JsonConvert.DeserializeObject<IList<Coin>>(content);
+                return coinList;
+            }
+        }
+
+        private static async Task<Coin> GetOneCoin(int id)
+        {
+            string Uri = "https://localhost:44347/api/coin/" + id;
+            using (HttpClient client = new HttpClient())
+            {
+                string content = await client.GetStringAsync(Uri);
+                Coin oneCoin = JsonConvert.DeserializeObject<Coin>(content);
+                return oneCoin;
+            }
+        }
+
+        public static async Task<HttpResponseMessage> GetCustomersAsyncAdd(int id, string genstand, int bud, string navn)
+        {
+            string CustomerUri = "https://localhost:44336/api/customer/";
+            using (HttpClient client = new HttpClient())
+            {
+                
+
+                var jsonString = JsonConvert.SerializeObject(new Coin(id, genstand, bud, navn));
+
+                StringContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(CustomerUri, content);
+                return response;
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IList<Coin> result = GetAllCoins().Result;
+            Console.WriteLine(result.Count);
+            foreach (Coin C in result)
+            {
+                Console.WriteLine(C.ToString());
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(GetOneCoin(1).Result);
+
+            Console.ReadLine();
         }
     }
 }
